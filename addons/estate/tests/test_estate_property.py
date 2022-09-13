2c7674623d7d9c8a5ef607e1d3ca80fe1904df08
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-09-12 21:53:48 -0300

    Update test of estate property model

--- addons/estate/tests/test_estate_property.py
+++ addons/estate/tests/test_estate_property.py
@@ -2,6 +2,8 @@ from odoo.tests.common import TransactionCase
 #from odoo.exceptions import UserError
 from odoo.tests import tagged
 from datetime import date
+from dateutil.relativedelta import relativedelta
+
 
 # The CI will run these tests after all the modules are installed,
 # not right after installing the one defining it.
@@ -16,13 +18,11 @@ class EstateTestCase(TransactionCase):
         # of in a setUp or in each test case, we reduce the testing time and
         # the duplication of code.
         cls.vals_0 = {
-            'name':'The Coach House	',
+            'name':'The Coach House',
             'description': 'A house that is situated above a row of garages or carports.',
             'postcode': '97300',
-            'date_availability':date.today(),
             'expected_price':1500000,
             'selling_price' :1800000,
-            'bedrooms' :5,
             'living_area' :250,
             'facades' : 6,
             'garage' :True,
@@ -32,6 +32,17 @@ class EstateTestCase(TransactionCase):
         }
         cls.properties_0 = cls.env['estate.property'].create([cls.vals_0])
 
+    def test_default_date_availability(self):
+        """Test that the method class _default_date_availability return the good value"""
+        self.assertEqual(
+            self.properties_0._default_date_availability(),
+            date.today() + relativedelta(months=3)
+        )
+
     def test_add_properties(self):
         """Test that the properties are correctly created."""
-        self.assertRecordValues(self.properties_0,[self.vals_0])
+        default_date_availability = date.today() + relativedelta(months=3)
+        default_values = {'bedrooms':2, 'date_availability':default_date_availability,'active':True,'state':'new'}
+        self.assertRecordValues(self.properties_0,[
+            {**self.vals_0, ** default_values}
+        ])
