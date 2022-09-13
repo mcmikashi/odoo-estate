d3dca3cf3e93abd5e2641b078214138d874e35e6
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-09-13 15:17:25 -0300

    Add new compute field for estate property offer

--- addons/estate/models/estate_property_offer.py
+++ addons/estate/models/estate_property_offer.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class EstatePropertyType(models.Model):
@@ -14,3 +14,19 @@ class EstatePropertyType(models.Model):
     )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = fields.Date.add(record.create_date, days=record.validity)
+            else:
+                record.date_deadline = fields.Date.add(fields.Date.today(), days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if fields.Date.add(record.create_date + record.validity) != record.date_deadline:
+                validity = record.date_deadline - record.create_date 
+                record.validity = validity.days
