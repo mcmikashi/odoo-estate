b2bf7d17dcd17f10c152207ba285cf222f847565
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-09-12 21:53:10 -0300

    Update estate property model

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -3,17 +3,26 @@
 from odoo import models, fields
 
 
-class estate(models.Model):
+class Estate(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
+
+    def _default_date_availability(self):
+        return fields.Date.add(fields.Date.today(), months=3)
+
+
 
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False,
+        default=lambda self: self._default_date_availability()
+    )
+    active = fields.Boolean(default=True)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -21,4 +30,8 @@ class estate(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('west', 'West'), ('east', 'East'), ('south', 'South')],
+    )
+    state = fields.Selection(
+        selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled','Canceled')],
+        default='new'
     )
