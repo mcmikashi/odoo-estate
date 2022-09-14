5c7ed1279e96805d4086c98e67c9eb72683ded38
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-09-14 13:52:51 -0300

    Add sequence to estate property type model, add sequence to the order and add sequence to the tree view

--- addons/estate/models/estate_property_type.py
+++ addons/estate/models/estate_property_type.py
@@ -6,7 +6,7 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Real estate property type"
-    _order = "name"
+    _order = "sequence, name"
 
     _sql_constraints = [
         ("check_name", "UNIQUE(name)", "The name must be unique"),
@@ -14,4 +14,4 @@ class EstatePropertyType(models.Model):
 
     name = fields.Char(required=True)
     property_ids = fields.One2many("estate.property", "property_type_id")
-
+    sequence = fields.Integer('Sequence', default=1, help="Used to order stages. Lower is better.")
