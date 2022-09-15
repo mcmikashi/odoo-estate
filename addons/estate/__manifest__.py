2149437b159fa8f93f3aeb64733718dad8cc7b65
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-09-15 17:11:56 -0300

    Add custom user view with property

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -27,6 +27,7 @@
         'views/estate_property_type_views.xml',
         'views/estate_property_tags_views.xml',
         'views/estate_property_offer_views.xml',
+        'views/estate_user_views.xml',
         'views/menu.xml',
         'views/templates.xml',
     ],
--- addons/estate/models/__init__.py
+++ addons/estate/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import estate_property, estate_property_type, estate_property_tags, estate_property_offer
+from . import estate_property, estate_property_type, estate_property_tags, estate_property_offer, estate_user
