8ef872f07fb050679db5080d31e1d73e4c63f24f
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-09-15 13:04:41 -0300

    Improve estate property type view and model
    *Allow user to see offers related to the estate propertu type

--- addons/estate/models/estate_property_offer.py
+++ addons/estate/models/estate_property_offer.py
@@ -22,6 +22,7 @@ class EstatePropertyType(models.Model):
     property_id = fields.Many2one("estate.property", required=True)
     validity = fields.Integer(default=7)
     date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+    property_type_id = fields.Many2one("estate.property.type", related="property_id.property_type_id", store=True)
 
     @api.depends("validity")
     def _compute_date_deadline(self):
