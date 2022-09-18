9e1bd23493820ad49c02e623b1083eab34182124
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-09-18 10:30:03 -0300

    Improvement allow user agent to create invoice

--- addons/estate_account/models/custom_property.py
+++ addons/estate_account/models/custom_property.py
@@ -4,8 +4,8 @@ class InheritedModel(models.Model):
     _inherit = "estate.property"
 
     def action_sell(self):
-        journal = self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal()
-        self.env["account.move"].create({
+        journal = self.env['account.move'].sudo().with_context(default_move_type='out_invoice')._get_default_journal()
+        self.env["account.move"].sudo().create({
             'partner_id':self.buyer_id,
             'move_type':'out_invoice',
             'journal_id':journal.id,
